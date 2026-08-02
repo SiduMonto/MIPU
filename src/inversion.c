8232f5a505b5a0fe4cfd2a538df8c8355eff2d64
SiduMonto <siddharthamontoya@gmail.com>
2026-08-02 21:43:33 +0200

    LA INVERSION COMPILA como puede haber sido tan dificil, pq hay 3 versiones de cada modulo

--- src/inversion.c
+++ src/inversion.c
@@ -1,73 +1,103 @@
 #include <zephyr/kernel.h>
-#include <nrfx_rtc.h>
-#include <nrfx_gpiote.h>
-#include <nrfx_ppi.h>
-#include <hal/nrf_gpio.h>
 #include <zephyr/logging/log.h>
 
+#include <drivers/nrfx_errors.h>
+#include <nrfx_rtc.h>
+#include <nrfx_gpiote.h>
+#include <helpers/nrfx_gppi.h>
+#include <hal/nrf_rtc.h>
+#include <hal/nrf_gpio.h>
 
 #include "inversion.h"
 
-LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(inversion, LOG_LEVEL_INF);
 
-static const nrfx_rtc_t rtc2 = NRFX_RTC_INSTANCE(2);
+static nrfx_gpiote_t gpiote_instance = NRFX_GPIOTE_INSTANCE(0);
+static nrfx_rtc_t rtc2 = NRFX_RTC_INSTANCE(2);
 
 #define EXTCOMIN_PIN NRF_GPIO_PIN_MAP(1, 12) // P1.12 en la Xiao BLE
 
 void init_hardware_vcom(void)
 {
-    nrfx_err_t err;
+    int err;
 
-	//GPIOTE
-    /* Configuramos el pin en modo Task para que cambie de estado (Toggle) */
-    nrfx_gpiote_out_config_t gpiote_config = NRFX_GPIOTE_CONFIG_OUT_TASK_TOGGLE(false);
-    
+    //GPIOTE
     /* Pedimos a Zephyr un canal GPIOTE libre para no pisar la configuración de los botones */
     uint8_t gpiote_channel;
-    err = nrfx_gpiote_channel_alloc(&gpiote_channel);
+    err = nrfx_gpiote_channel_alloc(&gpiote_instance, &gpiote_channel);
     if (err != NRFX_SUCCESS) {
-        LOG_ERR("Error: No GPIOTE channel available.\n");
+		LOG_ERR("Error: No GPIOTE channel available.\n");
+        return;
+    }
+	
+	/* Configuramos el pin en modo Task para que cambie de estado (Toggle) */
+    nrfx_gpiote_output_config_t output_config = {
+        .drive = NRF_GPIO_PIN_S0S1,
+        .input_connect = NRF_GPIO_PIN_INPUT_DISCONNECT,
+        .pull = NRF_GPIO_PIN_NOPULL,
+    };
+
+    nrfx_gpiote_task_config_t task_config = {
+        .task_ch = gpiote_channel,
+        .polarity = NRF_GPIOTE_POLARITY_TOGGLE,
+        .init_val = NRF_GPIOTE_INITIAL_VALUE_LOW,
+    };
+
+    /* Inicializamos el pin y activamos la tarea */
+    err = nrfx_gpiote_output_configure(&gpiote_instance, EXTCOMIN_PIN, &output_config, &task_config);
+    if (err != NRFX_SUCCESS) {
+        LOG_ERR("Error configuring GPIOTE output pin.\n");
         return;
     }
 
-    /* Inicializamos el pin y activamos la tarea */
-    nrfx_gpiote_out_init(EXTCOMIN_PIN, &gpiote_config);
-    nrfx_gpiote_out_task_enable(EXTCOMIN_PIN);
+    nrfx_gpiote_out_task_enable(&gpiote_instance, EXTCOMIN_PIN);
 
 
-	//RTC
+    //RTC
     nrfx_rtc_config_t rtc_config = NRFX_RTC_DEFAULT_CONFIG;
     rtc_config.prescaler = 0; //el preescaler lo dejo asi por ejemplo
     
     err = nrfx_rtc_init(&rtc2, &rtc_config, NULL); //NULL para el handler
+    if (err != NRFX_SUCCESS) {
+        LOG_ERR("Error initializing RTC2: %d\n", err);
+        return;
+    }
 
     /* Configuramos el canal de comparación 0. 
        Valor 32768 = salta cada 1 segundo (1Hz de inversión, suficiente para evitar quemado). 
-       El último parámetro 'false' evita que se genere una interrupción por software. */
+       El último parámetro 'false' para que no se genere una interrupción por software. */
     nrfx_rtc_cc_set(&rtc2, 0, 32768, false);
 
-	//el shorts para reiniciar a 0
-    nrf_rtc_shorts_enable(rtc2.p_reg, NRF_RTC_SHORT_COMPARE0_CLEAR_MASK);
+
+    //GPPI
+    nrfx_gppi_handle_t ppi_toggle_handle;
+    nrfx_gppi_handle_t ppi_clear_handle;
 
 
-   	//PPI
-    nrf_ppi_channel_t ppi_channel;
-    err = nrfx_ppi_channel_alloc(&ppi_channel);
-    if (err != NRFX_SUCCESS) {
-        LOG_ERR("Error: No PPI channel available.\n");
+    /* Obtenemos las direcciones de memoria */
+    uint32_t rtc_event_addr = nrfx_rtc_event_address_get(&rtc2, NRF_RTC_EVENT_COMPARE_0);
+    uint32_t gpiote_task_addr = nrfx_gpiote_out_task_address_get(&gpiote_instance, EXTCOMIN_PIN);
+    uint32_t rtc_task_clear_addr = nrfx_rtc_task_address_get(&rtc2, NRF_RTC_TASK_CLEAR);
+
+    // first connect RTC Event -> GPIOTE Toggle Task
+    err = nrfx_gppi_conn_alloc(rtc_event_addr, gpiote_task_addr, &ppi_toggle_handle);
+    if (err != 0) {
+        LOG_ERR("Failed to allocate GPPI toggle connection to GPIOTE\n");
         return;
     }
-    
-    /* Obtenemos las direcciones de memoria del evento (RTC) y la tarea (GPIOTE) */
-    uint32_t rtc_event_addr = nrfx_rtc_event_address_get(&rtc2, NRF_RTC_EVENT_COMPARE_0);
-    uint32_t gpiote_task_addr = nrfx_gpiote_out_task_address_get(EXTCOMIN_PIN);
 
-    //con el evento del rtc se activa la task del gpiote (que es toggle)
-    err = nrfx_ppi_channel_assign(ppi_channel, rtc_event_addr, gpiote_task_addr);
-    
-    err = nrfx_ppi_channel_enable(ppi_channel);
+    // then connect RTC Event -> RTC Clear Task (To loop back to 0)
+    err = nrfx_gppi_conn_alloc(rtc_event_addr, rtc_task_clear_addr, &ppi_clear_handle);
+    if (err != 0) {
+        LOG_ERR("Failed to allocate GPPI clear connection to RTC\n");
+        return;
+    }
 
-	
+    // Habilitamos las conexiones
+    nrfx_gppi_conn_enable(ppi_toggle_handle);
+    nrfx_gppi_conn_enable(ppi_clear_handle);
+    
+    //EMPEZAMOS
     nrfx_rtc_enable(&rtc2);
     
     LOG_INF("Hardware VCOM inversion initialized.\n");
